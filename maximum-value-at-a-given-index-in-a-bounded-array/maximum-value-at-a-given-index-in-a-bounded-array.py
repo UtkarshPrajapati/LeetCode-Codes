136b3541e3ac693ac304fc6b6afc237a8787a453
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-06-10 11:17:11 +0530

    Time: 59 ms (24.43%) | Memory: 16.4 MB (6.11%) - LeetSync

--- maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -1,16 +1,12 @@
 class Solution:
     def check(self, a):
-        lo=max(a-self.index,0)
-        res=(a+lo)*(a-lo+1)//2
-        ro=max(a-((self.n-1)-self.index),0)
-        res+=(a+ro)*(a-ro+1)//2
-        return res-a
+        lo,ro=max(a-self.index,0),max(a-((self.n-1)-self.index),0)
+        return (a+lo)*(a-lo+1)//2+(a+ro)*(a-ro+1)//2-a
     def maxValue(self, n, index, maxSum):
         self.n,self.index=n,index
         maxSum-=n
-        left,right=0,maxSum
-        while left<right:
-            mid=(left+right+1)//2
-            if self.check(mid)<=maxSum: left=mid
-            else: right=mid-1
-        return left+1
+        l,r=0,maxSum
+        while l<r:
+            mid=(l+r+1)//2
+            l,r=(mid,r) if self.check(mid)<=maxSum else (l,mid-1)
+        return l+1

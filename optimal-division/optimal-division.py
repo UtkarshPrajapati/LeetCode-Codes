23d00d3b58a07c2324bafa51b3a989e094e5d6b6
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-10 11:39:12 +0530

    Time: 38 ms (94.07%) | Memory: 16.2 MB (70.23%) - LeetSync

---
+++ optimal-division/optimal-division.py
@@ -0,0 +1,5 @@
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+        if len(nums)==1: return str(nums[0])
+        if len(nums)==2: return str(nums[0])+"/"+str(nums[1])
+        else: return f"{nums[0]}/({'/'.join(map(str,nums[1:]))})"

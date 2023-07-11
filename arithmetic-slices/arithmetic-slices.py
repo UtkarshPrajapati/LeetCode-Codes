c8ce697628054ba184e5b6e8d7753bdd6d59d737
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-11 15:04:36 +0530

    Time: 53 ms (62.10%) | Memory: 16.5 MB (66.92%) - LeetSync

--- arithmetic-slices/arithmetic-slices.py
+++ arithmetic-slices/arithmetic-slices.py
@@ -1,6 +1,4 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n,c,dp=len(nums),0,0
-        for i in range(2,n):
-            c+=(dp:=dp+1 if nums[i]+nums[i-2]==2*nums[i-1] else 0)
-        return c
+        dp=0
+        return sum(dp:=(dp+1)*(nums[i]+nums[i-2]==2*nums[i-1]) for i in range(2,len(nums)))

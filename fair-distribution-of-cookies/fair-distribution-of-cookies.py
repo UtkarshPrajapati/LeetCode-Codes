4c385bd32b56d312fe569a4f4d7d4aadf0a7c292
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-13 12:17:10 +0530

    Time: 9475 ms (14.36%) | Memory: 16.3 MB (83.62%) - LeetSync

--- fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,14 +1,14 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        min_unf = float('inf')
-        distribution = [0] * k
+        min_unf=float('inf')
+        d=[0]*k
         def backtrack(i):
-            nonlocal min_unf, distribution
-            if i == len(cookies): min_unf = min(min_unf, max(distribution));return
-            if min_unf <= max(distribution): return
+            nonlocal min_unf,d
+            if i == len(cookies): min_unf = min(min_unf, max(d));return
+            if min_unf <= max(d): return
             for j in range(k):
-                distribution[j] += cookies[i]
+                d[j] += cookies[i]
                 backtrack(i + 1)
-                distribution[j] -= cookies[i]
+                d[j] -= cookies[i]
         backtrack(0)
         return min_unf

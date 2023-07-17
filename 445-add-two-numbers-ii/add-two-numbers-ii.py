23c376aa37e4ac37d4e206d84787fe563c288776
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-17 10:02:33 +0530

    Time: 79 ms (76.22%) | Memory: 16.4 MB (68.70%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -1,9 +1,14 @@
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1,s2,d,c = [],[],ListNode(0),0
+    def addTwoNumbers(self,l1,l2):
+        s1,s2,d,c=[],[],ListNode(0),0
         while l1:s1.append(l1.val);l1=l1.next
         while l2:s2.append(l2.val);l2=l2.next
         while s1 or s2:
-            t=c+(s1.pop()if s1 else 0)+(s2.pop()if s2 else 0);c=t//10;n=ListNode(t%10);n.next=d.next;d.next=n
-        if c>0:n=ListNode(c);n.next=d.next;d.next=n
+            t=c+(s1.pop() if s1 else 0)+(s2.pop() if s2 else 0)
+            c=t//10
+            (n:=ListNode(t%10)).next=d.next
+            d.next=n
+        if c>0: 
+            (n:=ListNode(c)).next=d.next
+            d.next=n
         return d.next

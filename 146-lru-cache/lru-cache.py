a95bf0f302c8bd7f229e7ce44b1ae3a8c9503b10
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-18 10:45:51 +0530

    Time: 716 ms (85.33%) | Memory: 77.3 MB (89.34%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,35 +1,13 @@
 class LRUCache:
-    class N:
-        def __init__(s, k, v):
-            s.k, s.v, s.p, s.n = k, v, None, None
-    def __init__(s, c: int):
-        s.c, s.m = c, {}
-        s.h = s.t = s.N(-1, -1)
-        s.h.n = s.t
-        s.t.p = s.h
-    def aN(s, n):
-        t = s.h.n
-        n.n, n.p = t, s.h
-        s.h.n = t.p = n
-    def dN(s, n):
-        n.p.n = n.n
-        n.n.p = n.p
-    def get(s, k: int) -> int:
-        if (rN := s.m.get(k)):
-            a = rN.v
-            del s.m[k]
-            s.dN(rN)
-            s.aN(rN)
-            s.m[k] = s.h.n
-            return a
+    def __init__(self, capacity):
+        self.odict, self.capacity = OrderedDict(), capacity
+    def get(self, key):
+        if key in self.odict:
+            self.odict.move_to_end(key)
+            return self.odict[key]
         return -1
-    def put(s, k: int, v: int) -> None:
-        if k in s.m:
-            c = s.m[k]
-            del s.m[k]
-            s.dN(c)
-        if len(s.m) == s.c:
-            del s.m[s.t.p.k]
-            s.dN(s.t.p)
-        s.aN(s.N(k, v))
-        s.m[k] = s.h.n
+    def put(self, key, value):
+        if key in self.odict: self.odict[key] = value;self.odict.move_to_end(key)
+        else:
+            self.odict[key] = value
+            if len(self.odict) > self.capacity: self.odict.popitem(last=False)

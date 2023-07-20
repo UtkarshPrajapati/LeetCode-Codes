4eccaa0c38b40e899d05f0ee80c143da13ba6a48
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-20 09:50:43 +0530

    Time: 97 ms (97.48%) | Memory: 17.6 MB (57.73%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,10 @@
+class Solution:
+    def asteroidCollision(self, a: List[int]) -> List[int]:
+        stack = []
+        for i in a:
+            while stack and i < 0 < stack[-1]:
+                if stack[-1] < -i: stack.pop();continue
+                elif stack[-1] == -i:stack.pop()
+                break
+            else: stack.append(i)
+        return stack

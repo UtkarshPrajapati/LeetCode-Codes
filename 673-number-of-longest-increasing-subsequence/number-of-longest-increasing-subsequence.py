312d760086e5f5aa0c192d1ec4bf8d7068e9885d
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-21 08:49:59 +0530

    Time: 1034 ms (90.41%) | Memory: 16.6 MB (52.88%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -3,9 +3,9 @@ class Solution:
         n=len(nums);l=[1]*n;c=[1]*n
         for i in range(n):
             for j in range(i):
-                if nums[i] > nums[j]:
-                    if l[j] + 1 > l[i]:
-                        l[i] = l[j] + 1
-                        c[i] = c[j]
-                    elif l[j] + 1 == l[i]: c[i] += c[j]
-        return sum(c[i] for i in range(n) if l[i]==max(l))
+                if nums[i]>nums[j]:
+                    t=l[j]+1
+                    if t>l[i]: l[i],c[i]=t,c[j]
+                    elif t==l[i]: c[i]+=c[j]
+        m=max(l)
+        return sum(c[i] for i in range(n) if l[i] ==m)

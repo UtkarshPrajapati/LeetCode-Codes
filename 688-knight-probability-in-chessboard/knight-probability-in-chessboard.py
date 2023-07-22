ac3f8fc72de1cc829694e8614dd75f104a428c8d
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-22 10:29:35 +0530

    Time: 197 ms (87.89%) | Memory: 25 MB (46.73%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -4,6 +4,5 @@ class Solution:
         for r,c in product(ra:=range(n),ra):
             for dx,dy in ((-2,-1),(-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2)):
                 if 0<=(pos:=(r+dx,c+dy))[0]<n and 0<=pos[1]<n: l[(r,c)].append(pos)
-        @cache
-        def f(pos, h): return 1 if h==k else sum(f(next_pos,h+1) for next_pos in l[pos])
+        f=cache(lambda pos,h: 1 if h==k else sum(f(next_pos,h+1) for next_pos in l[pos]))
         return f((r0,c0),0)/8**k

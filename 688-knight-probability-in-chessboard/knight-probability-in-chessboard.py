95437364c1d341fd1f0d13be7bd76738b36b7987
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-22 10:09:07 +0530

    Time: 186 ms (91.28%) | Memory: 24.9 MB (46.73%) - LeetSync

---
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def knightProbability(self, n: int, k: int, r0: int, c0: int) -> float:
+        l=defaultdict(list)
+        for r,c in product(ra:=range(n),ra):
+            for dx,dy in ((-2,-1),(-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2)):
+                if 0<=(pos:=(r+dx,c+dy))[0]<n and 0<=pos[1]<n: l[(r,c)].append(pos)
+        @cache
+        def f(pos, h): return 1 if h==k else sum(f(next_pos,h+1) for next_pos in l[pos])
+        return f((r0,c0),0)/8**k

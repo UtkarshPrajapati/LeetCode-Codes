7b620582529ce8ed65399b7c2c2265bdfa5f8523
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-23 10:46:04 +0530

    Time: 179 ms (76.85%) | Memory: 20.8 MB (56.78%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -1,4 +1,3 @@
 class Solution:
     def allPossibleFBT(self, n: int):
-        dp={**{i:[] for i in range(0,n,2)},**{1:[TreeNode(0)]}}
-        return (bt:=cache(lambda n: dp[n] if n in dp else [TreeNode(0,l,r) for i in range(n) for l in bt(i) for r in bt(n-i-1)]))(n)
+        return (f:=cache(lambda n,dp=(lambda:{**{i:[] for i in range(0,n,2)},**{1:[TreeNode(0)]}})(): dp[n] if n in dp else [TreeNode(0,l,r) for i in range(n) for l in f(i) for r in f(n-i-1)]))(n)

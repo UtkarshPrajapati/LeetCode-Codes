3069bd70e70ade11c4c75ef6cd0f1167d40b7594
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-23 10:58:07 +0530

    Time: 165 ms (98.06%) | Memory: 19.7 MB (88.71%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -1,15 +1,6 @@
 class Solution:
     def allPossibleFBT(self, n: int) -> List[TreeNode]:
-        if n % 2 == 0:
-            return []
-        dp = [[] for _ in range(n + 1)]
-        dp[1] = [TreeNode(0)]
+        dp = [[], [TreeNode(0)]] + [[] for _ in range(n - 1)]
         for i in range(3, n + 1, 2):
-            for j in range(1, i, 2):
-                for left in dp[j]:
-                    for right in dp[i - j - 1]:
-                        root = TreeNode(0)
-                        root.left = left
-                        root.right = right
-                        dp[i].append(root)
+            dp[i] = [TreeNode(0, l, r) for j in range(1, i, 2) for l in dp[j] for r in dp[i - j - 1]]
         return dp[n]

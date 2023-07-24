dee9755a35b292e1b82f7a8eea0071e521b1cea3
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-24 08:28:36 +0530

    Time: 52 ms (26.00%) | Memory: 16.5 MB (7.14%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -2,6 +2,6 @@ class Solution:
     @cache
     def myPow(self, x: float, n: int) -> float:
         if not n: return 1
-        if n<0: return 1/self.myPow(x,-n)
-        t=self.myPow(x, n // 2);y=t*t
+        if n<0: x,n=1/x,-n
+        t=self.myPow(x,n//2);y=t*t
         return x*y if n&1 else y

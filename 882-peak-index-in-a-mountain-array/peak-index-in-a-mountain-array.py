d9295a1d0939af4736706a1fce051ceec594c55e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-25 08:01:49 +0530

    Time: 611 ms (26.90%) | Memory: 30.3 MB (32.70%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -1,5 +1,8 @@
-import numpy as np
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        arr=np.array(arr)
-        return np.where(arr==np.max(arr))[0][0]
+        def peakIndexInMountainArray(self,a):
+            l,r=0,len(a)-1 
+            while l<=r:
+                m=(l+r)//2
+                if a[m-1]<a[m]>a[m+1]: return m
+                if a[m+1]>a[m]: l=m+1
+                else: r=m

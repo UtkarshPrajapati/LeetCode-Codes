60a31eb44d7d8558083c545f0f4848e1952fea85
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-26 08:26:19 +0530

    Time: 1933 ms (99.15%) | Memory: 30.3 MB (98.58%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSpeedOnTime(self, d, t):
+        if (n:=len(d))>=t+1: return -1
+        i,j=1,max(max(d),ceil(d[n-1]/(t-n+1)))
+        while i<j:
+            m=(i+j)//2
+            if sum([ceil(d/m) for d in d[:-1]])+d[n-1]/m<=t: j=m
+            else: i=m+1
+        return j

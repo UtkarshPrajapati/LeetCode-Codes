e17f08c9af32608152ae9a9d40457f49789de41b
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-27 09:02:13 +0530

    Time: 530 ms (98.80%) | Memory: 30.6 MB (49.40%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -1,6 +1,10 @@
 class Solution:
     def maxRunTime(self, n: int, bat: List[int]) -> int:
-        i,j=1,sum(bat)//n 
+        s=sum(bat);i,j=1,s//n;l=len(bat)
+        if l<n: return 0
+        if l==n: return min(bat)
+        if n==1: return s
+        if j>=max(bat): return j
         while i<j: 
             t=j-(j-i)//2 
             if sum(min(b,t) for b in bat)<t*n: j=t-1 

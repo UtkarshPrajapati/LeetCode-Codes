d58ee28f0ad2c07add5a724c6e4d5331719d0ded
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-29 09:17:00 +0530

    Time: 43 ms (96.70%) | Memory: 16.8 MB (87.74%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -7,6 +7,6 @@ class Solution:
             if i<=0 and j<=0: return 0.5
             if i<=0: return 1
             if j<=0: return 0
-            d[(i,j)]=0.25*(dp(i-4,j)+dp(i-3,j-1)+dp(i-2,j-2)+dp(i-1,j-3))
+            d[(i,j)]=sum(dp(i+k-4,j-k) for k in range(4))/4
             return d[(i,j)]
         return dp(n,n)

b5793c571503d2db253ae16387aebc8bdbcb622b
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-29 10:26:14 +0530

    Time: 47 ms (89.15%) | Memory: 16.8 MB (91.98%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -2,6 +2,5 @@ class Solution:
     def soupServings(self, n: int) -> float:
         if n>4450: return 1
         d,n={},(n+24)//25
-        def dp(i,j):
-            return d[(i,j)] if (i,j) in d else 0.5 if i<=0 and j<=0 else 1 if i<=0 else 0 if j<=0 else d.setdefault((i,j),sum(dp(i+k-4,j-k) for k in range(4))/4)
+        dp=lambda i,j: d[(i,j)] if (i,j) in d else 0.5 if i<=0 and j<=0 else 1 if i<=0 else 0 if j<=0 else d.setdefault((i,j),sum(dp(i+k-4,j-k) for k in range(4))/4)
         return dp(n,n)

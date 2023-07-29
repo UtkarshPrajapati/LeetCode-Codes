ee9d2623f4365bb8dd331917c0fef8da80ed8cd2
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-29 10:15:07 +0530

    Time: 45 ms (92.92%) | Memory: 17 MB (79.25%) - LeetSync

--- 826-soup-servings/soup-servings.py
+++ 826-soup-servings/soup-servings.py
@@ -4,8 +4,5 @@ class Solution:
         d,n={},(n+24)//25
         def dp(i,j):
             if (i,j) in d: return d[(i,j)]
-            if i<=0 and j<=0: return 0.5
-            if i<=0: return 1
-            if j<=0: return 0
-            return d.setdefault((i,j),sum(dp(i+k-4,j-k) for k in range(4))/4)
+            return 0.5 if i<=0 and j<=0 else 1 if i<=0 else 0 if j<=0 else d.setdefault((i,j),sum(dp(i+k-4,j-k) for k in range(4))/4)
         return dp(n,n)

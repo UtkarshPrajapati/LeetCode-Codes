33f8b6bae49a81acd0fe6de31411b9dff5146f66
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-30 07:57:40 +0530

    Time: 253 ms (92.90%) | Memory: 19.3 MB (34.84%) - LeetSync

--- 664-strange-printer/strange-printer.py
+++ 664-strange-printer/strange-printer.py
@@ -1,10 +1,10 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        n=len(s);dp=[[0]*n for _ in range(n)]
-        for i in range(n-1,-1,-1):
-            dp[i][i]=1
-            for j in range(i+1,n):
-                dp[i][j]=dp[i][j-1]+1
-                for k in range(i,j):
-                    if s[k]==s[j]: dp[i][j]=min(dp[i][j],dp[i][k]+(dp[k+1][j-1]*(k+2<=j)))
-        return dp[0][n-1]
+        s="".join(k for k,_ in groupby(s))
+        @cache
+        def f(i,j):
+            if i==j: return 0
+            t=1+f(i+1,j)
+            if (l:=[f(i,m)+f(m+1,j) for m in range(i+1,j) if s[i]==s[m]]): t=min(t,min(l))
+            return t
+        return f(0,len(s))

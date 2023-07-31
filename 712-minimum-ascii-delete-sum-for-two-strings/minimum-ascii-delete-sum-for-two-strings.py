64fee78970b7d36b65c5f4f7dbebd79426b0dfd7
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-31 12:17:49 +0530

    Time: 1024 ms (33.98%) | Memory: 221.6 MB (10.34%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,11 +1,8 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        n1,n2,dp=len(s1),len(s2),{}
+        dp={}
         def f(i,j):
             if (i,j) in dp: return dp[(i,j)]
-            if i<0 and j<0: return 0
-            if i<0: dp[(i,j)] = ord(s2[j])+f(i,j-1)
-            elif j<0: dp[(i,j)] = ord(s1[i])+f(i-1,j)
-            else: dp[(i,j)] = f(i-1,j-1) if s1[i]==s2[j] else min(ord(s1[i])+f(i-1,j),ord(s2[j])+f(i,j-1))
+            dp[(i,j)]=0 if i<0 and j<0 else ord(s2[j])+f(i,j-1) if i<0 else ord(s1[i])+f(i-1,j) if j<0 else f(i-1,j-1) if s1[i]==s2[j] else min(ord(s1[i])+f(i-1,j),ord(s2[j])+f(i,j-1))
             return dp[(i,j)]
-        return f(n1-1,n2-1)
+        return f(len(s1)-1,len(s2)-1)

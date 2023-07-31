93ec7f704269ce2db962ce3de0b1137008fee0bd
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-31 12:31:33 +0530

    Time: 320 ms (98.75%) | Memory: 16.1 MB (99.77%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,8 +1,9 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        dp={}
-        def f(i,j):
-            if (i,j) in dp: return dp[(i,j)]
-            dp[(i,j)]=0 if i<0 and j<0 else ord(s2[j])+f(i,j-1) if i<0 else ord(s1[i])+f(i-1,j) if j<0 else f(i-1,j-1) if s1[i]==s2[j] else min(ord(s1[i])+f(i-1,j),ord(s2[j])+f(i,j-1))
-            return dp[(i,j)]
-        return f(len(s1)-1,len(s2)-1)
+        dp=[0]*len(s2)
+        for c1 in s1:
+            prev=0
+            for i,c2 in enumerate(s2):
+                last,dp[i]=dp[i],prev+ord(c1) if c1==c2 else max(dp[i],prev)
+                prev=max(prev,last)
+        return sum(map(ord,f'{s1}{s2}'))-2*max(dp)

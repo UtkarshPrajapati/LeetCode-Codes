4f005840a9b089009983f0ad06410c3923a6a212
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-07-31 11:53:50 +0530

    Time: 1031 ms (33.19%) | Memory: 221.7 MB (7.05%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,12 +1,11 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        n1,n2=len(s1),len(s2)
-        dp={}
+        n1,n2,dp=len(s1),len(s2),{}
         def f(i,j):
             if (i,j) in dp: return dp[(i,j)]
-            if i<0: dp[(i,j)] = sum(ord(ch) for ch in s2[:j+1])
-            elif j<0: dp[(i,j)] = sum(ord(ch) for ch in s1[:i+1])
-            elif s1[i]==s2[j]: dp[(i,j)] = f(i-1,j-1)
-            else: dp[(i,j)] = min(ord(s1[i])+f(i-1,j),ord(s2[j])+f(i,j-1))
+            if i<0 and j<0: return 0
+            if i<0: dp[(i,j)] = ord(s2[j])+f(i,j-1)
+            elif j<0: dp[(i,j)] = ord(s1[i])+f(i-1,j)
+            else: dp[(i,j)] = f(i-1,j-1) if s1[i]==s2[j] else min(ord(s1[i])+f(i-1,j),ord(s2[j])+f(i,j-1))
             return dp[(i,j)]
         return f(n1-1,n2-1)

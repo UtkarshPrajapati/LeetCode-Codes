44609d50cfdbc475095c55ec3592875a7833064b
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-01 23:20:14 +0530

    Time: 36 ms (94.75%) | Memory: 16.4 MB (36.83%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,3 +1,3 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        return " ".join([i[:-1] for i in sorted(s.split(), key=lambda x: x[-1])])
+        return " ".join(map(lambda x:x[:-1],sorted(s.split(),key=lambda x:x[-1])))

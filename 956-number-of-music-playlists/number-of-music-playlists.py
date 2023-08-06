e465d51f500cac24bcd7e52dce4cb3fc074bb079
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-06 09:54:37 +0530

    Time: 62 ms (67.67%) | Memory: 16.2 MB (93.23%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.py
+++ 956-number-of-music-playlists/number-of-music-playlists.py
@@ -2,8 +2,8 @@ class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
         dp=[[0 for _ in range(n+1)] for _ in range(2)];dp[0][0]=1
         for i in range(1,goal+1):
-            dp[i%2][0]=0
+            dp[i&1][0]=0
             for j in range(1,min(i,n)+1):
-                dp[i%2][j]=dp[(i-1)%2][j-1]*(n-(j-1))%1000000007
-                if j>k: dp[i%2][j]=(dp[i%2][j]+dp[(i-1)%2][j]*(j-k))%1000000007
-        return dp[goal%2][n]
+                dp[i&1][j]=dp[(i-1)&1][j-1]*(n-(j-1))%1000000007
+                if j>k: dp[i&1][j]=(dp[i&1][j]+dp[(i-1)&1][j]*(j-k))%1000000007
+        return dp[goal&1][n]

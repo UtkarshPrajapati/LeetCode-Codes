ada181b39a93337f1ae91b9e7977a0d4a251d41e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-14 07:59:27 +0530

    Time: 1488 ms (14.06%) | Memory: 29.6 MB (46.30%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,17 +1,13 @@
 class Solution:
     def findKthLargest(self,a,k):
         def partition(l,r):
-            p=a[r]
-            i=l
+            p,i=a[r],l
             for j in range(l,r):
-                if a[j]>p:
-                    a[i],a[j]=a[j],a[i]
-                    i+=1
+                if a[j]>p: a[i],a[j]=a[j],a[i];i+=1
             a[i],a[r]=a[r],a[i]
             return i
         l,r,k=0,len(a)-1,k-1
         while l<=r:
             pi=partition(l,r)
             if pi==k: return a[k]
-            elif pi<k: l=pi+1
-            else: r=pi-1
+            l,r=(pi+1,r) if pi<k else (l,pi-1)

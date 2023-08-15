e2902e80ebd9e465dda81d1d4a179323ccecc6e0
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-15 11:52:12 +0530

    Time: 50 ms (78.84%) | Memory: 16.3 MB (39.25%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,3 +1,4 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        return sum(i<j<k and nums[j]-nums[i]==diff and nums[k]-nums[j]==diff for i in range(len(nums)) for j in range(len(nums)) for k in range(len(nums)))
+        a=set(nums) 
+        return sum(i-diff in a and i+diff in a for i in nums)

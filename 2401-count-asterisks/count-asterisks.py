c3300701eb0917d6152aaa056e214837ccaf3e20
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-22 16:05:44 +0530

    Time: 35 ms (92.61%) | Memory: 16.3 MB (29.46%) - LeetSync

--- 2401-count-asterisks/count-asterisks.py
+++ 2401-count-asterisks/count-asterisks.py
@@ -2,6 +2,6 @@ class Solution:
     def countAsterisks(self, s: str) -> int:
         cbar,c=0,0
         for i in s:
-            if i=="|": cbar+=1
-            elif i=="*" and cbar%2==0: c+=1
+            cbar+=i=="|"
+            c+=i=="*" and cbar%2==0
         return c

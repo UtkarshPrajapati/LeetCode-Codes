115d1d0bd2ddd0b0c02a89efdf64e95da3e5c354
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-22 15:37:17 +0530

    Time: 136 ms (84.04%) | Memory: 16.9 MB (32.86%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -4,5 +4,5 @@ class Solution:
         mat=[[0]*(n-2) for _ in range(n-2)]
         for i in range(1,n-1):
             for j in range(1,n-1):
-                mat[i-1][j-1]=max(grid[i-1][j-1],grid[i-1][j],grid[i-1][j+1],grid[i][j-1],grid[i][j],grid[i][j+1],grid[i+1][j-1],grid[i+1][j],grid[i+1][j+1])
+                mat[i-1][j-1]=max(grid[i+x][j+y] for x in [-1, 0, 1] for y in [-1, 0, 1])
         return mat

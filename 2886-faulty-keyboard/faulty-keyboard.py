ed04d96f92c1410910ed5d6505838c48efb6f2b9
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-22 16:28:38 +0530

    Time: 61 ms (6.27%) | Memory: 16.2 MB (94.65%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.py
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -1,6 +1,6 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        a=[]
+        a=""
         for i in s:
-            a=a[::-1] if i=="i" else a+[i]
-        return "".join(a)
+            a=a[::-1] if i=="i" else a+i
+        return a

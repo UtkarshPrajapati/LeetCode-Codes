6c3a0666939d4bac4defe207aa1e00b6f8aace73
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-22 13:59:25 +0530

    Time: 44 ms (43.38%) | Memory: 16.2 MB (89.71%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -1,7 +1,3 @@
 class Solution:
     def convertToTitle(self,n):
-        a=[]
-        while n:
-            n,r=divmod(n-1,26)
-            a.append(chr(65+r))
-        return ''.join(reversed(a))
+        return "" if n==0 else self.convertToTitle((n-1)//26)+chr((n-1)%26+65)

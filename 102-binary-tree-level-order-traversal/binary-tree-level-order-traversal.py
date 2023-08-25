c1c3f594d7e100fd52b3e072c9c6ef7045010bf4
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-25 20:11:03 +0530

    Time: 40 ms (92.22%) | Memory: 18.8 MB (14.37%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -1,11 +1,14 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: return []
-        l=defaultdict(list)
+        l=[]
         def f(n,lev):
-            if not n: return
-            if n.left: l[lev].append(n.left.val);f(n.left,lev+1)
-            if n.right: l[lev].append(n.right.val);f(n.right,lev+1)
-        l[0].append(root.val)
+            if n.left: 
+                if lev==len(l): l.append([])
+                l[lev].append(n.left.val);f(n.left,lev+1)
+            if n.right: 
+                if lev==len(l): l.append([])
+                l[lev].append(n.right.val);f(n.right,lev+1)
+        l.append([root.val])
         f(root,1)
-        return list(l.values())
+        return l

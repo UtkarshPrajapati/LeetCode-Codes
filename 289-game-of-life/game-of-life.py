861b777584263694ab3f154f829cf1096dccb08f
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-25 14:56:09 +0530

    Time: 46 ms (50.70%) | Memory: 16.2 MB (88.81%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -3,8 +3,9 @@ class Solution:
         m,n=len(b),len(b[0])
         for i in range(m):
             for j in range(n):
-                c=sum(b[ii][jj]&1 for ii in range(max(0,i-1),min(i+2,m)) for jj in range(max(0,j-1),min(j+2,n)))
-                if c==3 or c-b[i][j]==3: b[i][j]|=2
+                s=sum(b[x][y]&1 for x in range(max(0,i-1),min(i+2,m)) for y in range(max(0,j-1),min(j+2,n)))
+                if b[i][j]==1 and (s==3 or s==4): b[i][j]|=2
+                if b[i][j]==0 and s==3: b[i][j]|=2
         for i in range(m):
             for j in range(n):
                 b[i][j]>>=1

1a0e5e5c7aecc177bb0176cc0f22f8d82549b835
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-27 00:32:18 +0530

    Time: 293 ms (87.00%) | Memory: 46.5 MB (18.28%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,21 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def sortList(self,head):
         if not head: return
-        s=[]
-        a=head
+        s,a=[],head
         while a:
             s.append(a.val)
             a=a.next
-        s.sort()
-        print(s)
-        ll=ListNode()
-        b=ll
+        s.sort(reverse=True)
+        b=ll=ListNode()
         while s:
-            ll.next=ListNode(s.pop(0))
+            ll.next=ListNode(s.pop())
             ll=ll.next
         return b.next

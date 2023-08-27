fc4bf741fc12789f2aff9edc94d95dd35487801c
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-28 00:47:32 +0530

    Time: 301 ms (97.57%) | Memory: 19.2 MB (63.26%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestIncreasingPath(self,mat):
+        m,n=len(mat),len(mat[0])
+        dp=[[0]*n for _ in range(m)]
+        def f(i,j):
+            if i<0 or i>=m or j<0 or j>=n: return 0
+            if dp[i][j]: return dp[i][j]
+            c=mat[i][j]
+            cnt=1
+            if j-1>=0 and c<mat[i][j-1]: cnt=max(cnt,f(i,j-1)+1)
+            if j+1<n and c<mat[i][j+1]: cnt=max(cnt,f(i,j+1)+1)
+            if i-1>=0 and c<mat[i-1][j]: cnt=max(cnt,f(i-1,j)+1)
+            if i+1<m and c<mat[i+1][j]: cnt=max(cnt,f(i+1,j)+1)
+            dp[i][j]=cnt
+            return cnt
+        a=0
+        for i in range(m):
+            for j in range(n):
+                a=max(a,f(i,j))
+        return a

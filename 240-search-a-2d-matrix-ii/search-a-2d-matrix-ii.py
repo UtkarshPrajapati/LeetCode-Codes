b90584c5bbabf0be08866bdad7345886a735be28
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-28 01:50:29 +0530

    Time: 146 ms (95.44%) | Memory: 22.7 MB (92.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,12 +1,9 @@
 class Solution:
     def searchMatrix(self,mat,t):
         m,n=len(mat),len(mat[0])
-        r,c=[],[]
-        for i in range(m):
-            if mat[i][0]<=t<=mat[i][n-1]: r.append(i)
-        for j in range(n):
-            if mat[0][j]<=t<=mat[m-1][j]: c.append(j)
-        for i in r:
-            for j in c:
-                if mat[i][j]==t: return True
+        r,c=0,n-1
+        while r<m and c>=0:
+            if mat[r][c]==t: return True
+            elif mat[r][c]>t: c-=1
+            else: r+=1
         return False

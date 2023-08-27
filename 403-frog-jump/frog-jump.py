5b845cc030aae827025893955abf5f8da21e67fe
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-27 11:26:24 +0530

    Time: 1485 ms (28.15%) | Memory: 48.3 MB (15.85%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -1,12 +1,13 @@
 class Solution:
-    def canCross(self, stones: List[int]) -> bool:
+    def canCross(self,stones):
         n=len(stones)
         dp=[[False]*(n+1) for _ in range(n)]
         dp[0][0]=True
         s=set(stones)
+        si={stone:i for i,stone in enumerate(stones)}
         for i in range(n-1):
             for j in range(n+1):
                 if dp[i][j]:
                     for k in [j-1,j,j+1]:
-                        if stones[i]+k in s: dp[stones.index(stones[i]+k)][k]=True
+                        if stones[i]+k in s: dp[si[stones[i]+k]][k]=True
         return any(dp[n-1])

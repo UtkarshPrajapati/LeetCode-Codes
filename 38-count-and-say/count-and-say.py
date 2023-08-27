03c3828d9f2e20bdd1e932a49e542fb13002e5a2
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-27 10:13:38 +0530

    Time: 36 ms (98.69%) | Memory: 16.4 MB (69.47%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,15 +1,13 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         if n==1: return "1"
+        if n==2: return "11"
         s=self.countAndSay(n-1)
-        last=s[0]
-        c=1
-        res=""
+        last,c,res=s[0],1,""
         for i in range(1,len(s)):
             if last==s[i]: c+=1
             else:
                 res+=str(c)+str(last)
-                last=s[i]
-                c=1
+                last,c=s[i],1
         res+=str(c)+str(last)
         return res

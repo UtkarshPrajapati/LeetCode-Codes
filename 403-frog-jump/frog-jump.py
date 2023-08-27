591e57097a002a0c1ffed1c49e87756716ea9410
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-27 11:23:41 +0530

    Time: 4292 ms (9.86%) | Memory: 47.9 MB (19.82%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -3,9 +3,10 @@ class Solution:
         n=len(stones)
         dp=[[False]*(n+1) for _ in range(n)]
         dp[0][0]=True
+        s=set(stones)
         for i in range(n-1):
             for j in range(n+1):
                 if dp[i][j]:
                     for k in [j-1,j,j+1]:
-                        if stones[i]+k in stones: dp[stones.index(stones[i]+k)][k]=True
+                        if stones[i]+k in s: dp[stones.index(stones[i]+k)][k]=True
         return any(dp[n-1])

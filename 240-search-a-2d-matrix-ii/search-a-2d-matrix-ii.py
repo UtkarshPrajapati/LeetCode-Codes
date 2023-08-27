721664ef7869868f3d2faff7cdb63317485d13a0
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-28 01:46:38 +0530

    Time: 167 ms (45.55%) | Memory: 22.7 MB (92.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -6,7 +6,6 @@ class Solution:
             if mat[i][0]<=t<=mat[i][n-1]: r.append(i)
         for j in range(n):
             if mat[0][j]<=t<=mat[m-1][j]: c.append(j)
-        print(r,c)
         for i in r:
             for j in c:
                 if mat[i][j]==t: return True

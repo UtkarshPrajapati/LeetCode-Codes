a3415acba7a42933420908ff2504543dacfd643e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-28 14:14:21 +0530

    Time: 1514 ms (12.31%) | Memory: 38 MB (90.70%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -4,9 +4,13 @@ class MedianFinder:
     def addNum(self,num):
         if not self.l: self.l.append(num)
         else:
-            for i in range(len(self.l)):
-                if num<self.l[i]: self.l.insert(i,num);break
-            else: self.l.append(num)
+            x,y=0,len(self.l)-1
+            while x<=y:
+                m=(x+y)//2
+                if self.l[m]==num: self.l.insert(m,num);break
+                elif self.l[m]<num: x=m+1
+                else: y=m-1
+            else: self.l.insert(x, num)
         self.c+=1
     def findMedian(self):
         return self.l[self.c//2] if self.c%2 else sum(self.l[self.c//2-1:self.c//2+1])/2

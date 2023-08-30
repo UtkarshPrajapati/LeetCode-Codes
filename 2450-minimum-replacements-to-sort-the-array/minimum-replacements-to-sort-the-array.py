1b35c59e48e95ff7633d465ba8538c6d82dd82c2
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-08-30 10:28:03 +0530

    Time: 471 ms (86.78%) | Memory: 31.3 MB (9.09%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
-        n=len(nums)
-        l,ans=nums[n-1],0
+        n,l,ans=len(nums),nums[-1],0
         for i in range(n-1,-1,-1):
             st=(nums[i]-1)//l
             l=nums[i]//(st+1)

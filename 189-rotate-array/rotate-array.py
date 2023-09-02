343781fa132ea9f145ebee9828d57e260570669c
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-02 19:37:35 +0530

    Time: 177 ms (86.80%) | Memory: 27.8 MB (68.32%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n=len(nums);a=[0]*n;k%=n
+        for i in range(n):
+            a[(i+k)%n]=nums[i]
+        nums[:]=a

3634047db3b57b266bef7b2318480273ca5b300c
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-04 23:24:25 +0530

    Time: 42 ms (68.82%) | Memory: 16.4 MB (51.80%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -2,7 +2,7 @@ class Solution:
     def rotate(self,mat):
         n=len(mat[0])
         for j in range(n):
-            for i in range(j,n):
+            for i in range(j+1,n):
                 mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
         for j in range(len(mat)):
             mat[j].reverse()

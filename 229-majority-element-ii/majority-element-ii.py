c811493972bb23392f436c3710435881935c342e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-05 18:31:43 +0530

    Time: 112 ms (69.07%) | Memory: 17.7 MB (61.48%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,6 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        d={}
-        for i in nums:
-            d[i]=d.get(i,0)+1
-        return [i for i in d if d[i]>len(nums)//3]
+        n=len(nums)
+        return [i for i,c in Counter(nums).items() if c>n//3]

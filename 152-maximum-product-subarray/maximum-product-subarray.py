6a8b7ebed078870cfad6f92cb7d6d82ed4a41625
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-07 17:44:13 +0530

    Time: 88 ms (39.45%) | Memory: 16.8 MB (58.94%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,9 +1,13 @@
 class Solution:
-    def maxProduct(self,arr):
-        n,pre,suf,ans=len(arr),1,1,float('-inf')
-        for i in range(n):
-            if pre==0: pre=1
-            if suf==0: suf=1
-            pre,suf=pre*arr[i],suf*arr[n-i-1]
-            ans=max([ans,pre,suf])
-        return ans
+    def maxProduct(self, arr):
+        p,mp=1,float('-inf')
+        for i in arr:
+            p*=i
+            mp=max(p, mp)
+            if p==0: p=1
+        p=1
+        for i in reversed(arr):
+            p*=i
+            mp=max(p,mp)
+            if p==0: p=1
+        return mp

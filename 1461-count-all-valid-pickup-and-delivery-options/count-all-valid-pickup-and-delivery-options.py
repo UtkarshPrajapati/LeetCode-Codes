4f59055944ab3600b8c2021e93ae4ffbc8cace8b
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-10 12:58:51 +0530

    Time: 43 ms (49.79%) | Memory: 16.4 MB (42.98%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
@@ -1,3 +1,6 @@
 class Solution:
-    def countOrders(self, n: int) -> int:
-        return (factorial(2*n)//2**n)%1000000007
+    def countOrders(self,n):
+        c=1
+        for i in range(2,n+1):
+            c*=(2*i-1)*i
+        return c%1000000007

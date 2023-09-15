7560bf10876aed53977b1633a4c08aeb2564da71
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-15 16:10:43 +0530

    Time: 61 ms (99.57%) | Memory: 16.4 MB (71.50%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestPalindrome(self,s):
+        if s==reversed(s): return s
+        start,size=0,1
+        for i in range(1,len(s)):
+            l,r=i-size,i+1
+            s1,s2=s[l-1:r],s[l:r]
+            if l>=1 and s1==s1[::-1]: size+=2;start=l-1
+            elif s2==s2[::-1]: size+=1;start=l
+        return s[start:start+size]

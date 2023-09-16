e7ac522625610daef4d2053e009190905e90a463
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-16 14:28:56 +0530

    Time: 52 ms (48.11%) | Memory: 16.8 MB (85.72%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,8 +1,11 @@
 class Solution:
     def searchMatrix(self,mat,t):
-        mat=[j for i in mat for j in i]
-        l,r=0,len(mat)-1
-        while l<=r:
-            if t==mat[(m:=(l+r)//2)]: return True
-            l,r=(m+1,r) if t>mat[m] else (l,m-1)
+        l,r,rn=0,len(mat[0])-1,0
+        while l<=r and 0<=rn<len(mat):
+            if mat[rn][l]<=t<=mat[rn][r]:
+                m=(l+r)//2
+                if mat[rn][m]==t: return True
+                elif mat[rn][m]>t: r=m-1
+                else: l=m+1
+            else: rn+=1
         return False

f01e30b0096276846b5bd92b1fcc1c55fa5cd695
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-09-17 22:09:32 +0530

    Time: 123 ms (79.12%) | Memory: 31.4 MB (85.57%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -1,8 +1,7 @@
 class Solution:
     def getIntersectionNode(self,headA,headB):
-        if headA and headB:
-            a,b=headA,headB
-            while a!=b:
-                a=a.next if a else headB
-                b=b.next if b else headA
-            return b
+        a,b=headA,headB
+        while a!=b:
+            a=a.next if a else headB
+            b=b.next if b else headA
+        return b

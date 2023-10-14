91542875f6b7475823a1fdf6d625aaadc6fa5915
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-10-14 14:37:07 +0530

    Time: 964 ms (74.81%) | Memory: 16.3 MB (95.93%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.py
+++ 2808-painting-the-walls/painting-the-walls.py
@@ -1,14 +1,7 @@
 class Solution:
-    def paintWalls(self,cost,time):
-        postfix_times=time.copy()
-        for i in range(len(postfix_times)-2,-1,-1):
-            postfix_times[i]+=postfix_times[i+1]
-        visited = {}
-        def dp(index,t):
-            if index==len(cost): return 0 if t>=0 else float("inf")
-            if t>=len(cost)-index: return 0
-            if t+postfix_times[index] < 0: return float("inf")
-            if (index,t) in visited: return visited[(index,t)]
-            visited[(index, t)] = min(dp(index+1,t-1),cost[index]+dp(index+1,t+time[index]))
-            return visited[(index,t)]
-        return dp(0,0)
+    def paintWalls(self, cost, time):
+        n=len(cost);dp=[0]+[sum(cost)]*n
+        for i,c in enumerate(cost):
+            for t in range(n,0,-1):
+                dp[t]=min(dp[t],dp[max(t-time[i]-1,0)]+c)
+        return dp[-1]

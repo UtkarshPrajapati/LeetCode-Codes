8e1fb0b33c420f57c7bdc7586bfae56ad3e8014e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-10-19 12:36:03 +0530

    Time: 38 ms (62.54%) | Memory: 16.1 MB (97.55%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,12 +1,9 @@
 class Solution:
     def backspaceCompare(self,s,t):
-        n,m,s1,s2,s,t=len(s),len(t),[],[],list(s),list(t)
-        for i in range(n):
-            a=s.pop(0)
-            if a!="#": s1.append(a)
-            elif s1: s1.pop()
-        for i in range(m):
-            b=t.pop(0)
-            if b!="#": s2.append(b)
-            elif s2: s2.pop()
-        return s1==s2
+        def f(s):
+            skip=0
+            for x in reversed(s):
+                if x=='#': skip+=1
+                elif skip: skip-=1
+                else: yield x
+        return all(x==y for x,y in zip_longest(f(s),f(t)))

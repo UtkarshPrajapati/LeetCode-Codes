2cf2b233cc9e6711a10efdb265576a589efa97b3
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-10-25 20:02:44 +0530

    Time: 38 ms (57.09%) | Memory: 16.3 MB (50.00%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -1,8 +1,6 @@
 class Solution:
-    def kthGrammar(self, n, k):
-        k -= 1
-        count = 0
+    def kthGrammar(self,n,k):
+        k,c=k-1,0
         while k:
-            k &= (k-1)
-            count += 1
-        return count & 1
+            k,c=k&(k-1),c+1
+        return c&1

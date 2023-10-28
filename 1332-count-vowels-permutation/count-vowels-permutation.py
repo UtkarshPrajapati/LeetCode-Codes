e0b785ecb5cd61d6807aef657dcba4b69ae7f2be
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-10-28 12:07:54 +0530

    Time: 82 ms (94.03%) | Memory: 16.2 MB (86.67%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.py
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -1,12 +1,6 @@
 class Solution:
-    def countVowelPermutation(self,n):
-        MOD = 10**9 + 7
-        dp = [[1]*5 for _ in range(n+1)]
-        for i in range(1,n+1):
-            dp[i][0]=dp[i-1][1]+dp[i-1][2]+dp[i-1][4]
-            dp[i][1]=dp[i-1][0]+dp[i-1][2]
-            dp[i][2]=dp[i-1][1]+dp[i-1][3]
-            dp[i][3]=dp[i-1][2]
-            dp[i][4]=dp[i-1][2]+dp[i-1][3]
-        # print(dp)
-        return sum(dp[n-1])%MOD
+    def countVowelPermutation(self, n):
+        MOD,a,e,i,o,u=1000000007,1,1,1,1,1
+        for _ in range(n-1):
+            a,e,i,o,u=(e+i+u)%MOD,(a+i)%MOD,(e+o)%MOD,i%MOD,(i+o)%MOD
+        return (a+e+i+o+u)%MOD

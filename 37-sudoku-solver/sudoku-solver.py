2924576c2dcf76d02e5cd45b54ecb83881f3109c
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-10-30 00:53:28 +0530

    Time: 243 ms (60.63%) | Memory: 16.2 MB (86.39%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -1,14 +1,26 @@
 class Solution:
-    def solveSudoku(self, b):
-        def f(b,row,col,c):
-            return not any(b[i][col]==c or b[row][i]==c or b[3*(row//3)+i//3][3*(col//3)+i%3]==c for i in range(9))
-        for i in range(9):
-            for j in range(9):
-                if b[i][j]==".":
-                    for c in "123456789":
-                        if f(b,i,j,c):
-                            b[i][j] = c
-                            if self.solveSudoku(b): return True
-                            b[i][j] = "."
-                    return False
-        return True
+    def solveSudoku(self,b):
+        rows, cols, triples, visit = [set() for _ in range(9)], [set() for _ in range(9)], [set() for _ in range(9)], [(i, j) for i in range(9) for j in range(9) if b[i][j]=="."]
+        for i,j in [(i,j) for i in range(9) for j in range(9) if b[i][j]!="."]:
+            e=b[i][j]
+            rows[i].add(e)
+            cols[j].add(e)
+            triples[(i//3)*3+j//3].add(e)
+        def dfs():
+            if not visit: return True
+            i,j=visit[0]
+            for num in "123456789":
+                if num not in rows[i]|cols[j]|triples[(i//3)*3+j//3]:
+                    visit.pop(0)
+                    rows[i].add(num)
+                    cols[j].add(num)
+                    triples[(i // 3) * 3 + j // 3].add(num)
+                    b[i][j] = num
+                    if dfs(): return True
+                    visit.insert(0, (i, j))
+                    rows[i].remove(num)
+                    cols[j].remove(num)
+                    triples[(i // 3) * 3 + j // 3].remove(num)
+                    b[i][j] = "."
+            return False
+        dfs()

ff765b695cfbb97b5fa9a597e82b34a7a7a0bc56
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-03 11:21:04 +0530

    Time: 31 ms (96.57%) | Memory: 16.2 MB (76.57%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -1,10 +1,10 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        ind=0
-        s=[]
-        for i in range(1,n+1):
-            if ind<len(target):
-                if target[ind]==i: s.append("Push");ind+=1
-                else: s.extend(["Push","Pop"])
-            else: break
+        s = []
+        for i in range(1, n+1):
+            if i in target:
+                s.append("Push")
+                target.remove(i)
+            else: s.extend(["Push","Pop"])
+            if not target: break
         return s

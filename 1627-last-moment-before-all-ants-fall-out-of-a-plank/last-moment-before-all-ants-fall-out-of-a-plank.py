c3b2f16d4f31a844de650be212e3d9710a05dcfb
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-04 11:02:10 +0530

    Time: 150 ms (44.60%) | Memory: 17.3 MB (30.22%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,3 +1,6 @@
 class Solution:
-    def getLastMoment(self,n,left,right):
-        return max(max(left,default=0),n-min(right,default=n))
+    def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
+        time = 0
+        if left: time = max(time, max(pos for pos in left))
+        if right: time = max(time, max(n-pos for pos in right))
+        return time

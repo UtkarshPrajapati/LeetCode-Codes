f962b3e8a0f55939a9d66f7d49a52652fa1a5157
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-08 00:18:41 +0530

    Time: 777 ms (10.59%) | Memory: 48.9 MB (5.43%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.py
@@ -1,6 +1,8 @@
+import numpy as np
 class Solution:
     def eliminateMaximum(self,dist,speed):
-        time,ans=sorted([(i/j) for i,j in zip(dist,speed)]),1
+        time=np.sort(np.array(dist)/np.array(speed))
+        ans=1
         for i,e in enumerate(time):
             if e<=i: return i
         return i+1

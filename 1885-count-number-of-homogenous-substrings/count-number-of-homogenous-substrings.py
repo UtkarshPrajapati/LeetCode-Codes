eaff4c764d672a6cd312b9536aceea4bf7d03c15
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-09 13:24:06 +0530

    Time: 84 ms (96.40%) | Memory: 17.5 MB (30.58%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -1,3 +1,3 @@
 class Solution:
     def countHomogenous(self,s):
-        return sum((n:=len(list(g)))*(n+1)//2 for k,g in groupby(s))%1000000007
+        return sum((n:=len(list(g)))*(n+1)//2 for _,g in groupby(s))%1000000007

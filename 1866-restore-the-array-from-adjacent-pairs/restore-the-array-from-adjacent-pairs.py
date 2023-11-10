04d7951fb68d9810c9f289c5159a56cbc91e716f
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-10 13:09:37 +0530

    Time: 945 ms (46.17%) | Memory: 69.6 MB (44.56%) - LeetSync

---
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def restoreArray(self,p):
+        n,d=len(p),defaultdict(list)
+        for a,b in p:
+            d[a].append(b)
+            d[b].append(a)
+        res=[[e for e,cnt in Counter([j for i in p for j in i]).items() if cnt==1][0]]
+        seen=set(res)
+        n1=1
+        while n1<n+1:
+            for ne in d[res[-1]]:
+                if ne not in seen: res.append(ne);seen.add(ne);n1+=1
+        return res

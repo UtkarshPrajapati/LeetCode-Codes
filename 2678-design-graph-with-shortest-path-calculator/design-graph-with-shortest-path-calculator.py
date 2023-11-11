e4120ec15dfcdd648fb2978b70d5c0ac7294d1f8
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-11 19:04:15 +0530

    Time: 1446 ms (24.12%) | Memory: 19.9 MB (17.65%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,28 @@
+class Graph:
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.adj = defaultdict(list)
+        for a, b, c in edges: self.adj[a].append((b, c))
+        self.n = n
+
+    def addEdge(self, edge: List[int]) -> None:
+        a, b, c = edge
+        self.adj[a].append((b, c))
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        dist = [float("inf")] * self.n
+        dist[node1] = 0
+        pq = [(0, node1)]
+        # visited = set()
+
+        while pq:
+            dis, node = heapq.heappop(pq)
+            # if node in visited:
+            #     continue
+            # visited.add(node)
+            for adjnode, weight in self.adj[node]:
+                newdist = dis + weight
+                if newdist < dist[adjnode]:
+                    dist[adjnode] = newdist
+                    heapq.heappush(pq, (newdist, adjnode))
+
+        return dist[node2] if dist[node2] != float("inf") else -1

7273749d397b4140b880f6850055b143c3358965
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-11 19:05:06 +0530

    Time: 842 ms (42.35%) | Memory: 19.7 MB (28.24%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
@@ -12,17 +12,14 @@ class Graph:
         dist = [float("inf")] * self.n
         dist[node1] = 0
         pq = [(0, node1)]
-        # visited = set()
-
+        visited = set()
         while pq:
             dis, node = heapq.heappop(pq)
-            # if node in visited:
-            #     continue
-            # visited.add(node)
+            if node in visited: continue
+            visited.add(node)
             for adjnode, weight in self.adj[node]:
                 newdist = dis + weight
                 if newdist < dist[adjnode]:
                     dist[adjnode] = newdist
                     heapq.heappush(pq, (newdist, adjnode))
-
         return dist[node2] if dist[node2] != float("inf") else -1

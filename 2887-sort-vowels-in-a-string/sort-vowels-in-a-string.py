5875d5b723fec43fba477b72af6275e08bfa5b9c
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-13 13:00:27 +0530

    Time: 110 ms (99.25%) | Memory: 18.8 MB (80.37%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,5 +1,5 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
         vowels = {"A","E","I","O","U","a","e","i","o","u"}
-        v = sorted([c for c in s if c in vowels])
-        return "".join([v.pop(0) if c in vowels else c for c in s])
+        v = deque(sorted([c for c in s if c in vowels]))
+        return "".join([v.popleft() if c in vowels else c for c in s])

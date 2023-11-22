b471f0c2274325f6b9f9f12b27f1371eb059ecab
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-22 14:43:29 +0530

    Time: 782 ms (70.09%) | Memory: 41.2 MB (35.88%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+    def findDiagonalOrder(self,nums):
         d = defaultdict(list)
         for i, row in enumerate(nums):
             for j, val in enumerate(row):

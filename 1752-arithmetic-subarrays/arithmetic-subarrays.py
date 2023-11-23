cb53ae104a8058761c5d96f614c685fe33c83c43
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-23 15:29:25 +0530

    Time: 202 ms (24.05%) | Memory: 16.6 MB (39.64%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -1,9 +1,6 @@
 class Solution:
     def checkArithmeticSubarrays(self,nums,l,r):
-        def f(arr):
+        def check(arr):
             arr.sort()
-            d=arr[1]-arr[0]
-            for i in range(2,len(arr)):
-                if arr[i]-arr[i-1]!=d: return False
-            return True
-        return [f(nums[l[i]:r[i]+1]) for i in range(len(l))]
+            return len(set(j-i for i,j in zip(arr,arr[1:])))==1
+        return [check(nums[i:j+1]) for i, j in zip(l, r)]

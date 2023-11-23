f67848a76b5c6ab3f74c22c2d9c9defe8960951e
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-23 15:18:03 +0530

    Time: 169 ms (47.66%) | Memory: 16.6 MB (39.64%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -3,7 +3,5 @@ class Solution:
         def f(arr):
             arr.sort()
             d=arr[1]-arr[0]
-            for i in range(2,len(arr)):
-                if arr[i]-arr[i-1]!=d: return False
-            return True
+            return all(arr[i]-arr[i-1]==d for i in range(2,len(arr)))
         return [f(nums[l[i]:r[i]+1]) for i in range(len(l))]

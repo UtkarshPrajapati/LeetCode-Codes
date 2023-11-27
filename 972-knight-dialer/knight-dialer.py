1a744421482fd5f06e967ed68dfc5f3fcfec66ca
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-11-27 17:25:48 +0530

    Time: 139 ms (95.73%) | Memory: 16.3 MB (91.46%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -1,6 +1,7 @@
 class Solution:
     def knightDialer(self,n):
-        d,dp=[[4,6],[6,8],[7,9],[4,8],[0,3,9],[],[0,1,7],[2,6],[1,3],[4,2]],[1]*10
-        for _ in range(n-1):
-            dp=[(sum(dp[j] for j in d[i])%1000000007) for i in range(10)]
-        return sum(dp)%1000000007
+        A,B,C,D=1,2,4,2
+        if n==1: return A+B+C+D+1
+        for i in range(n-1):
+            A,B,C,D=B,2*A+C,(B+D)*2,C
+        return (A+B+C+D)%1000000007

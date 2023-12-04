1af8dfed6e5b0e21e5143eca7a4c321d0f16551b
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-04 18:54:12 +0530

    Time: 42 ms (62.46%) | Memory: 16.2 MB (85.62%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -1,3 +1,6 @@
 class Solution:
-    def largestGoodInteger(self, n: str) -> str:
-        return max(n[i-2:i+1] if n[i] == n[i - 1] == n[i - 2] else "" for i in range(2, len(n)))
+    def largestGoodInteger(self,n):
+        m=""
+        for i in range(len(n)-2):
+            if n[i]==n[i+1]==n[i+2]: m=max(m,n[i:i+3])
+        return m

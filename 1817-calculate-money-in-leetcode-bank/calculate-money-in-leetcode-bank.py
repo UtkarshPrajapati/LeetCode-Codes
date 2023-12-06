afdda17865a43ccc77f38bde4f04170f34516f33
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-06 14:45:59 +0530

    Time: 34 ms (86.70%) | Memory: 16.2 MB (40.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,6 +1,4 @@
 class Solution:
     def totalMoney(self,n):
-        s,a,b=0,n//7,n%7
-        s+=7*(4*a+a*(a-1)//2)
-        s+=(a+1)*b+(b-1)*b//2
-        return s
+        a,b=n//7,n%7
+        return 7*(4*a+a*(a-1)//2)+(a+1)*b+(b-1)*b//2

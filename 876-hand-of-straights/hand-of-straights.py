128db2cdd5a2a8e581efcfa352abe32160e13a22
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-10 23:03:26 +0530

    Time: 548 ms (11.26%) | Memory: 17.9 MB (77.68%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,13 +1,13 @@
 class Solution:
-    def isNStraightHand(self, hand: List[int], s: int) -> bool:
+    def isNStraightHand(self,hand,s):
         n=len(hand)
         if n%s!=0: return False
         count=Counter(hand)
         while count:
             m=min(count)
             for k in range(m,m+s):
-                v=count[k]
-                if not v: return False
-                if v==1: del count[k]
+                v=count.get(k,0)
+                if v==0: return False
+                if v==1: count.pop(k)
                 else: count[k]=v-1
         return True

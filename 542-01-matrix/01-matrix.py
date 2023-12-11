6441e7fd7052bdbdecd176512bcc7833de8a2238
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-12 02:51:33 +0530

    Time: 533 ms (70.11%) | Memory: 19.1 MB (78.60%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,18 +1,16 @@
 class Solution:
     def updateMatrix(self,mat):
         if not mat or not mat[0]: return []
-        m,n=len(mat),len(mat[0])
-        queue = deque()
+        m,n,q=len(mat),len(mat[0]),deque()
         for i in range(m):
             for j in range(n):
-                if mat[i][j]==0: queue.append((i, j))
-                else: mat[i][j] = m*n
-        directions=[(1,0),(-1,0),(0,1),(0,-1)]
-        while queue:
-            row,col=queue.popleft()
-            for dr,dc in directions:
-                r,c=row+dr,col+dc
-                if 0<=r<m and 0<=c<n and mat[r][c]>mat[row][col]+1:
-                    queue.append((r,c))
-                    mat[r][c]=mat[row][col]+1
+                if mat[i][j]==0: q.append((i,j))
+                else: mat[i][j]=m*n
+        while q:
+            x,y=q.popleft()
+            for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nx,ny=x+dx,y+dy
+                if 0<=nx<m and 0<=ny<n and mat[nx][ny]>mat[x][y]+1:
+                    q.append((nx,ny))
+                    mat[nx][ny]=mat[x][y]+1
         return mat

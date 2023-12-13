94c1c8a253eef8df7a243e00e952192d8d2d93b5
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-13 16:00:55 +0530

    Time: 590 ms (49.74%) | Memory: 23.6 MB (80.80%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,7 +1,11 @@
 class Solution:
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        m,n,v=len(grid),len(grid[0]),set()
-        q=deque((i,j) for i in range(m) for j in range(n) if grid[i][j]==1 and (i in [0,m-1] or j in [0, n-1]))
+    def numEnclaves(self,grid):
+        m,n,v,q,c=len(grid),len(grid[0]),set(),deque(),0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    c+=1
+                    if i in [0,m-1] or j in [0, n-1]: q.append((i,j))
         while q:
             x,y=q.popleft()
             if (x,y) in v: continue
@@ -9,6 +13,4 @@ class Solution:
             for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
                 nx,ny=x+dx,y+dy
                 if 0<=nx<m and 0<=ny<n and grid[nx][ny]==1: q.append((nx,ny))
-        for x,y in v:
-            grid[x][y]=0
-        return sum(sum(row) for row in grid)
+        return c-len(v)

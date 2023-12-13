2a5cdeb79f9a2d881c7b67836a93632a05e5cdd3
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-14 02:54:47 +0530

    Time: 100 ms (85.27%) | Memory: 17.8 MB (38.08%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,15 +1,15 @@
 class Solution:
     def findCheapestPrice(self,n,flights,src,dst,k):
-        f=[[] for __ in range(n)]
-        for a,b,p in flights: f[a].append((b,p))
-        heap,d=[(0,src,k+1)],[[math.inf]*(k+2) for _ in range(n)]
-        while heap:
-            p,u,k=heappop(heap)
-            if u==dst: return p
-            if k>0:
-                for v,w in f[u]:
-                    nd=p+w
-                    if nd<d[v][k-1]:
-                        d[v][k-1]=nd
-                        heappush(heap,(nd,v,k-1))
-        return -1
+        adj=[[] for _ in range(n)]
+        for a,b,c in flights: adj[a].append((b,c))
+        dist,q,s=[float('inf')]*n,deque([(src,0)]),0
+        while q and s<=k:
+            for _ in range(len(q)):
+                node,pcost=q.popleft()
+                for neigh,cost in adj[node]:
+                    ncost=cost+pcost
+                    if ncost>=dist[neigh]: continue
+                    dist[neigh]=ncost
+                    q.append((neigh,ncost))
+            s+=1
+        return -1 if dist[dst]==float('inf') else dist[dst]

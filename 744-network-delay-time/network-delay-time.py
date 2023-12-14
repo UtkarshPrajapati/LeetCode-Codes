499d2674bd664f3dbe3bf38c838537802d839476
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-14 21:27:41 +0530

    Time: 408 ms (78.42%) | Memory: 18.9 MB (6.77%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -5,18 +5,11 @@ class Solution:
             adj[u-1].append((v-1, w))
         d = [float('inf')] * n
         d[k-1] = 0
-        tovis = [(0, k-1)]
+        tovis = deque([(0, k-1)])
         while tovis:
-            dis, node = heappop(tovis)
+            dis, node = tovis.popleft()
             for nei, w in adj[node]:
                 if dis + w < d[nei]:
                     d[nei] = dis + w
-                    heappush(tovis, (d[nei], nei))
-        # return max(d) if all(i != float('inf') for i in d) else -1
-        ans=-1
-        for i in d:
-            if i==float("inf"):
-                return -1
-            ans=max(ans,i)
-        return ans
-        
+                    tovis.append((d[nei], nei))
+        return max(d) if all(i != float('inf') for i in d) else -1

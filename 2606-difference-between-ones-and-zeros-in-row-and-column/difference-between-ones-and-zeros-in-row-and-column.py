185a32f30908005eac5eaa509d5a1d49517dc4f2
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-12-14 17:27:02 +0530

    Time: 1350 ms (83.57%) | Memory: 53.5 MB (27.86%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,8 +1,5 @@
 class Solution:
-    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        m,n=len(grid),len(grid[0])
-        r1=[sum(i) for i in grid]
-        r0=[m-i for i in r1]
-        c1=[sum(j) for j in zip(*grid)]
-        c0=[n-j for j in c1]
-        return [[r1[i]-r0[i]+c1[j]-c0[j] for j in range(n)] for i in range(m)]
+    def onesMinusZeros(self,grid):
+        m,n=len(grid),len(grid[0]);s=m+n
+        r1,c1=[sum(i) for i in grid],[sum(j) for j in zip(*grid)]
+        return [[2*(r1[i]+c1[j])-s for j in range(n)] for i in range(m)]

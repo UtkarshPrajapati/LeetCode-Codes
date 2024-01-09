358ae661d84b3159b7d1da73ca3103f664775e9e
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-01-09 12:46:59 +0530

    Time: 42 ms (44.05%) | Memory: 17.2 MB (28.86%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -1,22 +1,8 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        st1=[]
-        st2=[]
-        def f1(n):
-            if n.left: f1(n.left)
-            if not n.left and not n.right: st1.append(n.val);return
-            if n.right: f1(n.right)
-        def f2(n):
-            if n.left: f2(n.left)
-            if not n.left and not n.right: st2.append(n.val);return
-            if n.right: f2(n.right)
-        f1(root1)
-        f2(root2)
-        print(st1,st2)
-        return st1==st2
+        def f(n,st):
+            if n.left: f(n.left,st)
+            if not n.left and not n.right: st.append(n.val);return st
+            if n.right: f(n.right,st)
+            return st
+        return f(root1,[])==f(root2,[])

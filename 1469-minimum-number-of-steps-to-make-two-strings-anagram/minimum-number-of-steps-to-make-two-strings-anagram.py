cadfd04277efbb1dfb4186b4a6053b1e09097685
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-01-13 11:22:56 +0530

    Time: 142 ms (65.46%) | Memory: 18 MB (11.97%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,8 +1,9 @@
 class Solution:
     def minSteps(self,s,t):
-        c1,c2,ans=Counter(s),Counter(t),0
-        for ch in c1:
-            if ch in c2:
-                if c1[ch]>c2[ch]: ans+=c1[ch]-c2[ch]
-            else: ans+=c1[ch]
+        c,ans=[0]*26,0
+        for cs,ct in zip(s,t):
+            c[ord(cs)-97]+=1
+            c[ord(ct)-97]-=1
+        for count in c:
+            if count>0: ans+=count
         return ans

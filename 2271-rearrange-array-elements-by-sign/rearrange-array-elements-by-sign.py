fcf32e9f8e5f8bcdd15cb22b87e7d5716d669d32
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-02-14 20:13:03 +0530

    Time: 1017 ms (62.99%) | Memory: 47.7 MB (57.40%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,7 +1,7 @@
 class Solution:
     def rearrangeArray(self,A):
         n=len(A);ans,pi,ni=[0]*n,0,1
-        for i in range(n):
-            if A[i]<0: ans[ni]=A[i];ni+=2
-            else: ans[pi]=A[i];pi+=2
+        for i,ch in enumerate(A):
+            if ch<0: ans[ni]=ch;ni+=2
+            else: ans[pi]=ch;pi+=2
         return ans

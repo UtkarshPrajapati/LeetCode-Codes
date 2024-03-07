312567c09b2b07d34bec7b1d94b33f9284d4de75
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-03-07 13:08:28 +0530

    Time: 37 ms (45.21%) | Memory: 16.4 MB (99.36%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,5 +1,6 @@
 class Solution:
     def middleNode(self,head):
+        if head is None or head.next is None: return head
         slow=fast=head
         while fast and fast.next:
             slow,fast=slow.next,fast.next.next

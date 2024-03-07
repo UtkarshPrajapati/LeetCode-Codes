ec713ea7b50785d0ee14713f7cf1b3d722e29457
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-03-07 13:07:35 +0530

    Time: 39 ms (27.18%) | Memory: 16.6 MB (25.02%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,7 +1,6 @@
 class Solution:
     def middleNode(self,head):
-        slow,fast=head,head
+        slow=fast=head
         while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
+            slow,fast=slow.next,fast.next.next
         return slow

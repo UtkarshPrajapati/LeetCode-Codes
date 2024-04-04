63bdcd042722aaf1eec542c48b3d5b384c881f8c
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-04-04 11:34:28 +0530

    Time: 40 ms (21.18%) | Memory: 16.6 MB (38.30%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,9 +1,9 @@
 class Solution:
-    def maxDepth(self, s: str) -> int:
-        c,m=0,0
+    def maxDepth(self,s):
+        c,ans=0,0
         for i in s:
             if i=='(':
                 c+=1
-                if c>m: m=c
+                if c>ans: ans=c
             elif i==')': c-=1
-        return m
+        return ans

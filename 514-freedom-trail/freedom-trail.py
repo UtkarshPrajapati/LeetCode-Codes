3c50163f511602252212a4dcfa5856a6e23d466d
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-04-27 15:57:42 +0530

    Time: 113 ms (63.14%) | Memory: 16.8 MB (59.22%) - LeetSync

--- 514-freedom-trail/freedom-trail.py
+++ 514-freedom-trail/freedom-trail.py
@@ -2,12 +2,12 @@ class Solution:
     def findRotateSteps(self,ring,key):
         memo,positions={},defaultdict(list)
         for i,c in enumerate(ring): positions[c].append(i)
-        return self.dp(0,0,positions,key,ring,memo)
-    def dp(self,ki,ri,pos,key,ring,memo):
-        if ki==len(key): return 0
-        if (ki,ri) in memo: return memo[(ki,ri)]
-        steps=float('inf')
-        for i in pos[key[ki]]:
-            steps=min(steps,min((i-ri)%len(ring),(ri-i)%len(ring))+self.dp(ki+1,i,pos,key,ring,memo))
-        memo[(ki,ri)]=steps+1
-        return steps+1
+        def dp(ki,ri,pos,key,memo):
+            if ki==len(key): return 0
+            if (ki,ri) in memo: return memo[(ki,ri)]
+            steps=float('inf')
+            for i in pos[key[ki]]:
+                steps=min(steps,min((i-ri)%len(ring),(ri-i)%len(ring))+dp(ki+1,i,pos,key,memo))
+            memo[(ki,ri)]=steps+1
+            return steps+1
+        return dp(0,0,positions,key,memo)

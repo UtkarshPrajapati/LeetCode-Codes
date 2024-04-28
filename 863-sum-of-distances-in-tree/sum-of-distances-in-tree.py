6339a515eb1427355fbf421afbd2f4b394d3d37b
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-04-28 13:45:42 +0530

    Time: 855 ms (24.66%) | Memory: 47.6 MB (20.33%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumOfDistancesInTree(self, N, edges):
+        tree = collections.defaultdict(set)
+        res, count = [0] * N, [1] * N
+        for i, j in edges:
+            tree[i].add(j)
+            tree[j].add(i)
+        def dfs(root, pre):
+            for i in tree[root]:
+                if i != pre:
+                    dfs(i, root)
+                    count[root] += count[i]
+                    res[root] += res[i] + count[i]
+        def dfs2(root, pre):
+            for i in tree[root]:
+                if i != pre:
+                    res[i] = res[root] - count[i] + N - count[i]
+                    dfs2(i, root)
+        dfs(0, -1)
+        dfs2(0, -1)
+        return res

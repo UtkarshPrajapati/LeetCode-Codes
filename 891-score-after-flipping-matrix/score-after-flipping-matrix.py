65a154b12f1276e18e4fdc63a8b7f9fadd5de96b
Utkarsh Prajapati <utkarshprap@gmail.com>
2024-05-13 20:52:27 +0530

    Time: 41 ms (56.47%) | Memory: 16.7 MB (18.53%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,10 +1,7 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         m,n=len(grid),len(grid[0])
-        for i in range(m):
-            if grid[i][0]==0:
-                for j in range(n):
-                    grid[i][j]=1-grid[i][j]
+        grid=[[1-val for val in row] if row[0]==0 else row for row in grid]
         for j in range(1,n):
             if sum(grid[i][j] for i in range(m))<m/2:
                 for i in range(m):
